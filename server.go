863bc127fa1ac057870256491d65114bc94b6f81
Shonali-Pawde <84071365+Shonali-Pawde@users.noreply.github.com>
2021-05-13 22:43:58 -0700

    change port number

--- server.go
+++ server.go
@@ -39,7 +39,7 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8082", nil); err != nil {
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
 }
