cabe84ed9662b6803045d3296f958148e76ddc5a
Shonali-Pawde <84071365+Shonali-Pawde@users.noreply.github.com>
2021-05-13 22:01:19 -0700

    port commit

--- server.go
+++ server.go
@@ -43,7 +43,7 @@ func main() {
 
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
 }
